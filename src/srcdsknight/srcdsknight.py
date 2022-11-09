97a57104b03e0179959d6ffcf97c51dcced10f95
maxime1907 <19607336+maxime1907@users.noreply.github.com>
2022-11-09 22:36:51 +0100

    feat: format

--- src/srcdsknight/srcdsknight.py
+++ src/srcdsknight/srcdsknight.py
@@ -1,18 +1,27 @@
 from __future__ import annotations
+
 import logging
 import os
 import shutil
-import requests
-import click
 import tarfile
-
 from typing import Any, Dict
 
+import click
+import requests
 import yaml
 
 logger = logging.getLogger(__name__)
 
-def download_dependency(*, filename: str, fileextension: str, cache: bool, cachePath: str, downloadURL: str, skipFailedDownload: bool) -> bool:
+
+def download_dependency(
+    *,
+    filename: str,
+    fileextension: str,
+    cache: bool,
+    cachePath: str,
+    downloadURL: str,
+    skipFailedDownload: bool,
+) -> bool:
     if os.path.exists(f"{cachePath}/{filename}") and cache:
         logger.info(f"Skipping existing cache for {fileextension} {downloadURL}")
     else:
@@ -27,7 +36,16 @@ def download_dependency(*, filename: str, fileextension: str, cache: bool, cache
             raise exc
     return True
 
-def extract_dependency(*, linkName: str, filename: str, cache: bool, cachePath: str, packagePath: str, skipFailedExtract: bool) -> bool:
+
+def extract_dependency(
+    *,
+    linkName: str,
+    filename: str,
+    cache: bool,
+    cachePath: str,
+    packagePath: str,
+    skipFailedExtract: bool,
+) -> bool:
     if os.path.exists(f"{packagePath}/{linkName}") and cache:
         logger.info(f"Skipping existing cache for folder {linkName}")
     else:
@@ -40,11 +58,16 @@ def extract_dependency(*, linkName: str, filename: str, cache: bool, cachePath: 
                 return False
             raise exc
 
-        my_tar.extractall(f"{packagePath}/{linkName}") # specify which folder to extract to
+        my_tar.extractall(
+            f"{packagePath}/{linkName}"
+        )  # specify which folder to extract to
         my_tar.close()
     return True
 
-def copy_folder_content(*, extractPath: str, subPath: str, installPath: str, directories: Dict[str, Any]) -> None:
+
+def copy_folder_content(
+    *, extractPath: str, subPath: str, installPath: str, directories: Dict[str, Any]
+) -> None:
     source_dir = f"{extractPath}"
     if subPath:
         source_dir = f"{source_dir}/{subPath}"
@@ -58,15 +81,33 @@ def copy_folder_content(*, extractPath: str, subPath: str, installPath: str, dir
             if os.path.exists(source_subdir):
                 shutil.copytree(source_subdir, desination_subdir, dirs_exist_ok=True)
 
-def install_dependency(*, extractPath: str, installPath: str, folders: Dict[str, Any], directories: Dict[str, Any]) -> None:
+
+def install_dependency(
+    *,
+    extractPath: str,
+    installPath: str,
+    folders: Dict[str, Any],
+    directories: Dict[str, Any],
+) -> None:
     for folderName, folderConfig in folders.items():
         if folderName == "gametype":
-            copy_folder_content(extractPath=extractPath, subPath=folderConfig["path"], installPath=installPath, directories=directories)
+            copy_folder_content(
+                extractPath=extractPath,
+                subPath=folderConfig["path"],
+                installPath=installPath,
+                directories=directories,
+            )
         elif folderName == "common":
             for path in folderConfig["paths"]:
-                copy_folder_content(extractPath=extractPath, subPath=path, installPath=installPath, directories=directories)
+                copy_folder_content(
+                    extractPath=extractPath,
+                    subPath=path,
+                    installPath=installPath,
+                    directories=directories,
+                )
 
-def install_dependencies(*, config: Dict[str, any]) -> None:
+
+def install_dependencies(*, config: Dict[str, Any]) -> None:
     folders = config['folders']
     directories = folders['directories']
     installPath = os.path.abspath(config['installPath'])
@@ -93,20 +134,40 @@ def install_dependencies(*, config: Dict[str, any]) -> None:
         fileextension = "tar.gz"
         filename = f'{linkName}.{fileextension}'
         cache = config["cache"]["enabled"]
-        skipFailedDownload =  config["skipFailedDownload"]
-        skipFailedExtract =  config["skipFailedDownload"]
+        skipFailedDownload = config["skipFailedDownload"]
+        skipFailedExtract = config["skipFailedDownload"]
 
-        if download_dependency(filename=filename, fileextension=fileextension, cache=cache, cachePath=cachePath, downloadURL=downloadURL, skipFailedDownload=skipFailedDownload) and extract_dependency(linkName=linkName, filename=filename, cache=cache, cachePath=cachePath, packagePath=packagePath, skipFailedExtract=skipFailedExtract):
-            install_dependency(extractPath=f"{packagePath}/{linkName}", installPath=installPath, folders=folders, directories=directories)
+        if download_dependency(
+            filename=filename,
+            fileextension=fileextension,
+            cache=cache,
+            cachePath=cachePath,
+            downloadURL=downloadURL,
+            skipFailedDownload=skipFailedDownload,
+        ) and extract_dependency(
+            linkName=linkName,
+            filename=filename,
+            cache=cache,
+            cachePath=cachePath,
+            packagePath=packagePath,
+            skipFailedExtract=skipFailedExtract,
+        ):
+            install_dependency(
+                extractPath=f"{packagePath}/{linkName}",
+                installPath=installPath,
+                folders=folders,
+                directories=directories,
+            )
             shutil.rmtree(f"{packagePath}/{linkName}")
 
     if os.path.exists(packagePath):
         shutil.rmtree(packagePath)
 
+
 @click.command()
 @click.option('--config-file', default="config.yml", help='Configuration file path.')
 @click.option('-v', '--verbose', count=True, help="Verbosity level")
-def cli(config_file: str, verbose: int):
+def cli(config_file: str, verbose: int) -> None:
 
     log_level = logging.ERROR
     if verbose > 2:
